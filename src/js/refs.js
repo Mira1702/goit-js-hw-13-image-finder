370857b0c86e74c27329469328d2d73d0fd6c606
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-01-08 22:19:54 +0200

    Updates

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-    searchForm: document.querySelector('.search-form'),
-    searchFormInput: document.querySelector('.search-form-input'),
-    galleryList: document.querySelector('.gallery'),
-    loadMoreBtn: document.querySelector('[data-action="load more"]')
-};
-export default refs;
